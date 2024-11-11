135819a03d1980c1990d475b5e1713c867ba0190
Ivan Obraztcov <ivan.obraztcov@yahoo.com>
2024-11-11 23:13:42 +0400

    4 Ivan Obraztcov

---
+++ ConsoleAppC#/Labs7/Labs7/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Labs7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book[] books = new Book[3];
+            books[0] = new Book();
+            books[0].SetBook("Author1", "Title1", 200, 2000);
+            books[1] = new Book();
+            books[1].SetBook("Author2", "Title2", 300, 1990);
+            books[2] = new Book();
+            books[2].SetBook("Author3", "Title3", 250, 2010);
+
+            Array.Sort(books);
+
+            foreach (Book book in books)
+            {
+                book.Show();
+            }
+        }
+    }
+
+    public class Book : IComparable
+    {
+        private string author;
+        private string title;
+        private int year;
+        private int pages;
+
+        public void SetBook(string author, string title, int pages, int year)
+        {
+            this.author = author;
+            this.title = title;
+            this.pages = pages;
+            this.year = year;
+        }
+        public void Show()
+        {
+            Console.WriteLine("\nКнига:\n Автор: {0}\n Название: {1}\n Год издания: {2}\n {3} стр.\n", author, title, year, pages);
+        }
+
+        public int CompareTo(object obj)
+        {
+            Book it = (Book)obj;
+            if (this.year == it.year) return 0;
+            else if (this.year > it.year) return 1;
+            else return -1;
+        }
+    }
+}
---
+++ ConsoleAppC#/Labs8/Labs8/Program.cs
@@ -0,0 +1,112 @@
+﻿namespace Labs8
+{
+    using System;
+
+    class Triangle
+    {
+        private double sideA;
+        private double sideB;
+        private double sideC;
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            this.sideA = sideA;
+            this.sideB = sideB;
+            this.sideC = sideC;
+
+            if (!IsValidTriangle(sideA, sideB, sideC))
+            {
+                throw new ArgumentException("Треугольник с такими сторонами не существует.");
+            }
+        }
+
+        public Triangle(double side)
+        {
+            this.sideA = side;
+            this.sideB = side;
+            this.sideC = side;
+        }
+
+        public double GetPerimeter()
+        {
+            return sideA + sideB + sideC;
+        }
+
+        public double GetArea()
+        {
+            double semiPerimeter = GetPerimeter() / 2;
+            return Math.Sqrt(semiPerimeter * (semiPerimeter - sideA) * (semiPerimeter - sideB) * (semiPerimeter - sideC));
+        }
+        
+        private bool IsValidTriangle(double sideA, double sideB, double sideC)
+        {
+            return (sideA + sideB > sideC) && (sideA + sideC > sideB) && (sideB + sideC > sideA);
+        }
+
+        public void DisplaySides()
+        {
+            Console.WriteLine($"Стороны треугольника: {sideA}, {sideB}, {sideC}");
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выберите тип треугольника:");
+            Console.WriteLine("1. Треугольник с заданными длинами сторон");
+            Console.WriteLine("2. Равносторонний треугольник");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            try
+            {
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Введите длину стороны A:");
+                        double a = double.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Введите длину стороны B:");
+                        double b = double.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Введите длину стороны C:");
+                        double c = double.Parse(Console.ReadLine());
+
+                        Triangle triangle1 = new Triangle(a, b, c);
+                        Console.WriteLine("Треугольник 1:");
+                        triangle1.DisplaySides();
+                        Console.WriteLine("Периметр: " + triangle1.GetPerimeter());
+                        Console.WriteLine("Площадь: " + triangle1.GetArea());
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Введите длину стороны равностороннего треугольника:");
+                        double side = double.Parse(Console.ReadLine());
+
+                        Triangle triangle2 = new Triangle(side);
+                        Console.WriteLine("Треугольник 2:");
+                        triangle2.DisplaySides();
+                        Console.WriteLine("Периметр: " + triangle2.GetPerimeter());
+                        Console.WriteLine("Площадь: " + triangle2.GetArea());
+                        break;
+
+                    default:
+                        Console.WriteLine("Некорректный выбор.");
+                        break;
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Некорректный ввод. Пожалуйста, введите число.");
+            }
+        }
+
+    }
+
+}
