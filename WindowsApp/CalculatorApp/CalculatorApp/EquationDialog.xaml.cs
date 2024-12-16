a4236f13d49dc488472075731f972a94afa1143f
Ivan Obraztcov <ivan.obraztcov@yahoo.com>
2024-12-16 23:15:28 +0400

    Ivan Obraztcov

---
+++ WindowsApp/CalculatorApp/CalculatorApp/EquationDialog.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+
+namespace CalculatorApp
+{
+    public partial class EquationDialog : Window
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+
+        public EquationDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void SolveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(AInput.Text, out double a) &&
+                double.TryParse(BInput.Text, out double b) &&
+                double.TryParse(CInput.Text, out double c))
+            {
+                A = a;
+                B = b;
+                C = c;
+                this.DialogResult = true;
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Введите корректные значения для коэффициентов.");
+            }
+        }
+    }
+}
---
+++ WindowsApp/CalculatorApp/CalculatorApp/VisualTreeHelperExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+public static class VisualTreeHelperExtensions
+{
+    public static IEnumerable<T> FindVisualChildren<T>(DependencyObject parent) where T : DependencyObject
+    {
+        if (parent == null) yield break;
+
+        for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
+        {
+            var child = VisualTreeHelper.GetChild(parent, i);
+            if (child is T)
+            {
+                yield return (T)child;
+            }
+
+            foreach (var descendant in FindVisualChildren<T>(child))
+            {
+                yield return descendant;
+            }
+        }
+    }
+}
+
