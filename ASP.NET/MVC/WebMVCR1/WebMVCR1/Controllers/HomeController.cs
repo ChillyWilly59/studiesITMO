9173f9ea6d906cdf5a9d883518cbd6ca12e4ba95
Ivan <ivan.obraztcov@yahoo.com>
2025-04-03 22:39:14 +0400

    MVC-Razor

---
+++ ASP.NET/MVC/WebMVCR1/WebMVCR1/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebMVCR1.Models;
+
+namespace WebMVCR1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static PersonRepository db = new PersonRepository();
+
+        //public string Index(string hel) 
+        //{
+        //    string Greeting = ModelClass.ModelHello() + ", " + hel; 
+        //    return Greeting;
+        //}
+
+
+        public ViewResult Index() 
+        { 
+            int hour = DateTime.Now.Hour; 
+            ViewBag.Greeting = hour < 12 ? "Доброе утро" : "Добрый день"; 
+            ViewData["Mes"] = "хорошего настроения"; 
+            return View(); 
+        }
+        [HttpPost]
+        public ViewResult InputData(Person p)
+        {
+            db.AddResponse(p);
+            return View("Hello", p);
+        }
+        [HttpGet]
+        public ViewResult InputData()
+        {
+            return View();
+        }
+        public ViewResult OutputData() 
+        { 
+            ViewBag.Pers = db.GetAllResponses; 
+            ViewBag.Count = db.NumberOfPerson; 
+            return View("ListPerson"); 
+        }
+    }
+}
---
+++ ASP.NET/MVC/WebMVCR1/WebMVCR1/Models/PersonRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace WebMVCR1.Models
+{
+    public class PersonRepository
+    {
+        private static List<Person> persons = new();
+        public int NumberOfPerson { get { return persons.Count(); } }
+        public IEnumerable<Person> GetAllResponses { get { return persons; } }
+
+        public void AddResponse(Person pers) { persons.Add(pers); }
+
+    }
+}
