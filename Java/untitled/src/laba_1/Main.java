8618a0c889e26e81c1ec125d488907fdd3626bdd
Ivan Obraztcov <ivan.obraztcov@yahoo.com>
2025-06-09 22:24:36 +0400

    lad 1

---
+++ Java/untitled/src/laba_1/Main.java
@@ -0,0 +1,21 @@
+package laba_1;
+
+public class Main
+{
+    public static void main(String[] args) {
+        int result1 = ((46+10)*(10/3));
+        int result2 = (29)*(4)*(-15);
+
+        int number = 10500;
+        int result = (number/10)/10;
+
+        double x = 3.6;
+        double y = 4.1;
+        double z = 5.9;
+
+        System.out.println("Я\nхорошо\nзнаю\nJava ");
+        System.out.println(String.format("(46+10)*(10/3) = %s\n(29)*(4)*(-15) = %s",result1,result2));
+        System.out.println(result);
+        System.out.println(x*y*z);
+    }
+}
