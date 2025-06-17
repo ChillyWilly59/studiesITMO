caa00570af8249812306906c53a79c898f938d73
Ivan Obraztcov <ivan.obraztcvo@yahoo.com>
2025-06-17 22:04:35 +0400

    second lab fix
    Signed-off-by: Ivan Obraztcov <ivan.obraztcvo@yahoo.com>

--- Java/untitled/src/laba_2/Book.java
+++ Java/untitled/src/laba_2/Book.java
@@ -1,26 +1,62 @@
 package laba_2;
 
-public class Book
-{
+public class Book {
     private String title;
     private String author;
     private int year;
     private int pages;
 
 
-    public Book()
-    {
-        title = " ";
+    public Book() {
+        title = "";
         author = "";
         year = 0;
         pages = 0;
     }
 
-    public Book(String title,String author, int year,int pages)
-    {
+    public Book(String title, String author, int year, int pages) {
         title = title;
         author = author;
         year = year;
         pages = pages;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public toString() {
+        return "Книга: " + title + "\n" +
+                "Автор: " + author + "\n" +
+                "Год издания: " + year + "\n" +
+                "Количество страниц: " + pages;
     }
 }
